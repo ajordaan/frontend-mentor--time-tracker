121eaa45e25880dcd21cfc66032db83a4f437f0d
Andrew Jordaan <ajordaan23@gmail.com>
2021-11-08 19:25:57 +0200

    Added stuff to activity card

--- src/components/ActivityCard.vue
+++ src/components/ActivityCard.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="wrapper">
+  <div class="card">
     <div class="icon-background">
       <img :src="iconName" />
+    </div>
+    <div class="card-body">
+      <h4>{{ activity.title }}</h4>
+      <h3>{{ activity.timeframes[currentTimeframe].current }}hrs</h3>
     </div>
   </div>
 </template>
@@ -15,10 +19,22 @@ export default {
 
   computed: {
     iconName() {
-      return "@/assets/icon-" + this.activity.title.toLowerCase() + ".svg";
+      console.log("BASE URL: " + process.env.BASE_URL);
+      console.log("Title: " + this.activity.title.toLowerCase());
+      return `${
+        process.env.BASE_URL
+      }assets/icon-${this.activity.title.toLowerCase().replace(" ", "-")}.svg`;
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.card-body {
+  background-color: hsl(235, 46%, 20%);
+}
+
+.icon-background {
+  background-color: hsl(15, 100%, 70%);
+}
+</style>
