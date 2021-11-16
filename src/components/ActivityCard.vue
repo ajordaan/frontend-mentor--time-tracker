43c840969be0052d61c0b3c33ca79aaf008d14e2
Andrew Jordaan <andrew.jordaan@platform45.com>
2021-11-16 20:17:42 +0200

    Added some styling to the activity card

--- src/components/ActivityCard.vue
+++ src/components/ActivityCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="card">
-    <div :style="`background-color: ${activity.colour} `">
-      <img :src="iconName" />
+  <div>
+    <div class="card-header rounded-corners" :style="`background-color: ${activity.colour} `">
+      <img class="card-image" :src="iconName" />
     </div>
-    <div class="card-body">
-      <h4>{{ activity.title }}</h4>
-      <h3>{{ activity.timeframes[currentTimeframe].current }}hrs</h3>
+    <div class="card-body rounded-corners">
+      <h4 class="card-title">{{ activity.title }}</h4>
+      <h3 class="current-hours">{{ activity.timeframes[currentTimeframe].current }}hrs</h3>
       <p>Last Week - {{activity.timeframes[currentTimeframe].previous }}hrs </p>
     </div>
   </div>
@@ -31,8 +31,36 @@ export default {
 </script>
 
 <style scoped>
+
+.rounded-corners {
+  border-radius: 10px;
+}
+.card-header {
+  height: 100px;
+  position: relative;
+  bottom: -4rem;
+  z-index: -1;
+  display: flex;
+  justify-content: right;
+}
+
+.card-image {
+  height: 50px;
+  padding-right: 0.5rem;
+}
 .card-body {
   background-color: hsl(235, 46%, 20%);
+  padding: 2rem;
+  text-align: left;
+}
+
+.card-title {
+  padding-bottom: 2rem;
+}
+
+.current-hours {
+  font-size: 3rem;
+  padding-bottom: 1rem;
 }
 
 .icon-background {
