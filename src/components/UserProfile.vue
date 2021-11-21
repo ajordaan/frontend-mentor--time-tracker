4b6961d94f1e07e3e033f39458e51df8ce88e596
Andrew Jordaan <andrew.jordaan@platform45.com>
2021-11-21 21:39:56 +0200

    Styling updates

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,8 +2,10 @@
   <div class="user-profile-wrapper">
     <div ref="userInfo" class="user-info rounded-corners">
       <img src="@/assets/image-jeremy.png" />
-      <h5 class="label">Report for</h5>
-      <h2 class="title">Jeremy Robson</h2>
+      <div class="userTitleAndLabel">
+        <h5 class="label">Report for</h5>
+        <h2 class="title">Jeremy Robson</h2>
+      </div>
     </div>
     <div ref="timeframe" class="timeframe rounded-corners">
       <ul class="label">
@@ -52,14 +54,16 @@ export default {
 }
 .user-info {
   background-color: hsl(246, 80%, 60%);
-  padding: 2rem 4rem 4rem 2rem;
+  padding: 1rem;
   position: relative;
   z-index: 2;
+  display: flex;
+  align-items: center;
 }
 
 .timeframe {
   background-color: hsl(235, 46%, 20%);
-  padding: 4rem 4rem 2rem 2rem;
+  padding: 4rem 2rem 2rem 2rem;
   position: relative;
   top: -2rem;
 }
@@ -70,19 +74,18 @@ export default {
 }
 
 .timeframe li {
-  padding-bottom: 1rem;
   list-style-type: none;
 }
 
 .user-info .title {
-  font-size: 3rem;
+  font-size: 1.5rem;
 }
 
 .user-info img {
-  height: 4rem;
+  height: 5rem;
   border: 2px solid #fff;
   border-radius: 50%;
-  margin-bottom: 2.5rem;
+  margin-right: 1rem;
 }
 
 .label {
@@ -101,5 +104,25 @@ li:hover {
   .timeframe ul {
     flex-direction: column;
   }
+ .user-info .title {
+   font-size: 3rem;
+  }
+
+  .user-info img {
+  margin-bottom: 2rem;
+}
+
+.user-info {
+  padding: 2rem 4rem 4rem 2rem;
+  display: block;
+}
+
+.timeframe {
+  padding: 4rem 4rem 2rem 2rem;
+}
+
+.timeframe li {
+  padding-bottom: 1rem;
+}
 }
 </style>
